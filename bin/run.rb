9ee7309fe76d30a3df06e26016c3265ae04261fc
github username <github email address>
2018-06-26 16:08:47 -0400

    added continous loop

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,13 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+input = ""
+flag = true
+while (flag)
+  character = get_character_from_user
+  if character == "quit"
+    flag=false
+  else
+    show_character_movies(character)
+  end
+end
